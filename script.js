fa145fc30a4e58b2b0779126054f827529aa20dc
Andrew Kovpak <andrew.kovpak.01@gmail.com>
2023-06-01 17:57:04 +0300

    optim js

--- script.js
+++ script.js
@@ -1,39 +1,79 @@
-window.addEventListener("scroll", function () {
-    let navbar = document.querySelector(".header");
-    if (
-        document.body.scrollTop > 200 ||
-        document.documentElement.scrollTop > 200
-    ) {
-        navbar.classList.add("shrink");
-    } else {
-        navbar.classList.remove("shrink");
-    }
-});
+// window.addEventListener("scroll", function () {
+//     let navbar = document.querySelector(".header");
+//     if (
+//         document.body.scrollTop > 200 ||
+//         document.documentElement.scrollTop > 200
+//     ) {
+//         navbar.classList.add("shrink");
+//     } else {
+//         navbar.classList.remove("shrink");
+//     }
+// });
+
+// const buttonToUp = document.querySelector(".button__scroll");
+
+// window.addEventListener("scroll", () => {
+//     if (window.pageXOffset > 200 || document.documentElement.scrollTop > 200) {
+//         buttonToUp.classList.add("visible");
+//     } else {
+//         buttonToUp.classList.remove("visible");
+//     }
+// });
+
+// buttonToUp.addEventListener("click", () => {
+//     window.scrollTo({
+//         top: 0,
+//         behavior: "smooth",
+//     });
+// });
+
+// const gif = document.querySelector(".gif-container");
+
+// window.addEventListener("scroll", () => {
+//     let scroll = this.scrollY;
+//     if (scroll === 0) {
+//         gif.style.left = scroll - 125 + "px";
+//     } else if (scroll > 0) {
+//         gif.style.left = scroll - 35 + "px";
+//     }
+// });
 
 const buttonToUp = document.querySelector(".button__scroll");
+const gif = document.querySelector(".gif-container");
+const navbar = document.querySelector(".header");
 
-window.addEventListener("scroll", () => {
+// function shrink() {
+//     if (
+//         document.body.scrollTop > 200 ||
+//         document.documentElement.scrollTop > 200
+//     ) {
+//         navbar.classList.add("shrink");
+//     } else {
+//         navbar.classList.remove("shrink");
+//     }
+// }
+
+function visible() {
     if (window.pageXOffset > 200 || document.documentElement.scrollTop > 200) {
         buttonToUp.classList.add("visible");
+        navbar.classList.add("shrink");
     } else {
         buttonToUp.classList.remove("visible");
+        navbar.classList.remove("shrink");
     }
-});
+}
 
-buttonToUp.addEventListener("click", () => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-    });
-});
-
-const gif = document.querySelector(".gif-container");
-
-window.addEventListener("scroll", () => {
+function scroll() {
     let scroll = this.scrollY;
     if (scroll === 0) {
         gif.style.left = scroll - 125 + "px";
     } else if (scroll > 0) {
         gif.style.left = scroll - 35 + "px";
     }
+}
+
+window.addEventListener("scroll", function () {
+    // shrink();
+    visible();
+    scroll();
 });
