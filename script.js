c645685953ac672aa90e7813a48c6d0c254862f8
Andrew Kovpak <andrew.kovpak.01@gmail.com>
2023-06-02 11:02:06 +0300

    clr js

--- script.js
+++ script.js
@@ -1,59 +1,8 @@
-// window.addEventListener("scroll", function () {
-//     let navbar = document.querySelector(".header");
-//     if (
-//         document.body.scrollTop > 200 ||
-//         document.documentElement.scrollTop > 200
-//     ) {
-//         navbar.classList.add("shrink");
-//     } else {
-//         navbar.classList.remove("shrink");
-//     }
-// });
-
-// const buttonToUp = document.querySelector(".button__scroll");
-
-// window.addEventListener("scroll", () => {
-//     if (window.pageXOffset > 200 || document.documentElement.scrollTop > 200) {
-//         buttonToUp.classList.add("visible");
-//     } else {
-//         buttonToUp.classList.remove("visible");
-//     }
-// });
-
-// buttonToUp.addEventListener("click", () => {
-//     window.scrollTo({
-//         top: 0,
-//         behavior: "smooth",
-//     });
-// });
-
-// const gif = document.querySelector(".gif-container");
-
-// window.addEventListener("scroll", () => {
-//     let scroll = this.scrollY;
-//     if (scroll === 0) {
-//         gif.style.left = scroll - 125 + "px";
-//     } else if (scroll > 0) {
-//         gif.style.left = scroll - 35 + "px";
-//     }
-// });
-
 const buttonToUp = document.querySelector(".button__scroll");
 const gif = document.querySelector(".gif-container");
 const navbar = document.querySelector(".header");
 
-// function shrink() {
-//     if (
-//         document.body.scrollTop > 200 ||
-//         document.documentElement.scrollTop > 200
-//     ) {
-//         navbar.classList.add("shrink");
-//     } else {
-//         navbar.classList.remove("shrink");
-//     }
-// }
-
-function visible() {
+function changeHeader() {
     if (window.pageXOffset > 200 || document.documentElement.scrollTop > 200) {
         buttonToUp.classList.add("visible");
         navbar.classList.add("shrink");
@@ -63,7 +12,7 @@ function visible() {
     }
 }
 
-function scroll() {
+function progressScrollbar() {
     let scroll = this.scrollY;
     if (scroll === 0) {
         gif.style.left = scroll - 125 + "px";
@@ -73,7 +22,6 @@ function scroll() {
 }
 
 window.addEventListener("scroll", function () {
-    // shrink();
-    visible();
-    scroll();
+    changeHeader();
+    progressScrollbar();
 });
